bc631e5fa0e9609198f794b17e48886846c19f95
SATHISH-ASUS\sathi <sathish662001@gmail.com>
2024-10-29 01:46:13 +0530

    fix(cors): add localhost as whitelist domain

--- config/config.ts
+++ config/config.ts
@@ -2,5 +2,5 @@ export default {
   port: process.env.PORT ?? 4000,
   host: process.env.HOST ?? "localhost",
   nodeEnv: process.env.NODE_ENV ?? "",
-  whiteListedOrigin: process.env.WHITE_LISTED_ORIGIN ?? "",
+  whiteListedOrigin: process.env.WHITE_LISTED_ORIGIN ?? "http://localhost:3000",
 };
